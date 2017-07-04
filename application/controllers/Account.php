27c2250a859d1c50dd6e6636bb4cbb2e32b3fe12
Kamas "Iceberg" Lau <kamaslau@outlook.com>
2017-07-05 04:59:01 +0800

    beta20170705
    [add]Account类check_stuff方法
    [change]非客户端类终端在登录时可获取相关员工信息

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -46,6 +46,32 @@
 			if ( !empty($user_info) ):
 				// 更新最后登录信息
 				@$this->basic_model->edit($user_info['user_id'], $login_info);
+				
+				// 非客户端登录时，检查该用户是否为员工
+				if ($this->app_type !== 'client'):
+					$stuff = $this->check_stuff( $user_info['user_id'] );
+
+					if ( !empty($stuff) ):
+						// 不允许商家员工登录管理端
+						if ($this->app_type === 'admin' && !empty($stuff['biz_id']) ):
+							$this->result['status'] = 415;
+							$this->result['content']['error']['message'] = '该用户并非管理端员工';
+							exit();
+
+						// 不允许管理员工登录非管理端
+						elseif ($this->app_type !== 'admin' && empty($stuff['biz_id']) ):
+							$this->result['status'] = 415;
+							$this->result['content']['error']['message'] = '该用户并非商户端员工';
+							exit();
+						
+						else:
+							$user_info['biz_id'] = $stuff['biz_id'];
+							$user_info['role'] = $stuff['role'];
+							$user_info['level'] = $stuff['level'];
+
+						endif;
+					endif;
+				endif;
 
 				// 不返回真实密码信息
 				if ( !empty($user_info['password']) ) $user_info['password'] = 'set';
@@ -277,10 +303,36 @@
 						$login_info['last_login_ip'] = empty($this->input->post('user_ip'))? $this->input->ip_address(): $this->input->post('user_ip'); // 优先检查请求是否来自APP
 						$login_info['last_login_timestamp'] = time();
 						@$this->basic_model->edit($user_info['user_id'], $login_info);
+						
+						// 非客户端登录时，检查该用户是否为员工
+						if ($this->app_type !== 'client'):
+							$stuff = $this->check_stuff( $user_info['user_id'] );
+
+							if ( !empty($stuff) ):
+								// 不允许商家员工登录管理端
+								if ($this->app_type === 'admin' && !empty($stuff['biz_id']) ):
+									$this->result['status'] = 415;
+									$this->result['content']['error']['message'] = '该用户并非管理端员工';
+									exit();
+
+								// 不允许管理员工登录非管理端
+								elseif ($this->app_type !== 'admin' && empty($stuff['biz_id']) ):
+									$this->result['status'] = 415;
+									$this->result['content']['error']['message'] = '该用户并非商户端员工';
+									exit();
+								
+								else:
+									$user_info['biz_id'] = $stuff['biz_id'];
+									$user_info['role'] = $stuff['role'];
+									$user_info['level'] = $stuff['level'];
+
+								endif;
+							endif;
+						endif;
 
 						// 不返回真实密码信息
 						if ( !empty($user_info['password']) ) $user_info['password'] = 'set';
-						
+
 						$this->result['status'] = 200;
 						$this->result['content'] = array_merge($user_info, $login_info);
 
@@ -366,8 +418,6 @@
 
 		/**
 		 * 短信验证
-		 *
-		 * TODO 改为实例化并调用Sms::verify()
 		 */
 		private function verify_sms()
 		{
@@ -410,8 +460,8 @@
 				$this->basic_model->table_name = 'sms'; // 更改当前表名
 				$this->basic_model->id_name = 'sms_id'; // 更改当前表主键名
 				$items = $this->basic_model->select($condition, NULL, FALSE, FALSE);
-				$this->basic_model->table_name = 'user'; // 还原当前表名
-				$this->basic_model->id_name = 'user_id'; // 还原当前表主键名
+				$this->basic_model->table_name = $this->table_name; // 还原当前表名
+				$this->basic_model->id_name = $this->id_name; // 还原当前表主键名
 
 				if ( empty($items) ):
 					$this->result['status'] = 400;
@@ -421,11 +471,10 @@
 				endif;
 
 			endif;
-		}
+		} // end verify_sms
 
 		/**
 		 * 创建用户
-		 * TODO 改为实例化并调用User::create()
 		 *
 		 * @params $data_to_create 待创建的用户信息
 		 * @params $name 用于创建用户的资料类型；手机号mobile，电子邮箱Email，默认mobile
@@ -438,7 +487,7 @@
 			// 动态设置待验证字段名及字段值
 			$data_to_validate["{$name}"] = $data_to_create[$name];
 			$this->form_validation->set_data($data_to_validate);
-			$this->form_validation->set_rules('mobile', '手机号', 'trim|required|exact_length[11]|is_natural|is_unique[user.mobile]');
+			$this->form_validation->set_rules('mobile', '手机号', 'trim|required|exact_length[11]|is_natural_no_zero|is_unique[user.mobile]');
 			//$this->form_validation->set_rules('email', 'Email', 'trim|required|max_length[40]|valid_email|is_unique[user.email]');
 
 			// 若表单提交不成功
@@ -452,7 +501,7 @@
 				return $this->basic_model->create($data_to_create, TRUE);
 
 			endif;
-		}
+		} // end user_create
 
 		/**
 		 * 检查是否已经有以相应手机号注册的账户
@@ -470,7 +519,7 @@
 			else:
 				return ( empty($result) )? FALSE: TRUE;
 			endif;
-		}
+		} // end check_mobile
 
 		/**
 		 * 检查是否已经有以相应Email注册的账户
@@ -488,8 +537,29 @@
 			else:
 				return ( empty($result) )? FALSE: TRUE;
 			endif;
-		}
-	}
+		} // end check_email
+
+		// 检查是否已经有与相应user_id相关的员工记录
+		private function check_stuff($user_id, $return_boolean = FALSE)
+		{
+			$this->basic_model->table_name = 'stuff';
+			$this->basic_model->id_name = 'stuff_id';
+
+			$data_to_search['user_id'] = $user_id;
+			$data_to_search['time_delete'] = NULL; // 仅获取有效的员工关系
+			$result = $this->basic_model->match($data_to_search);
+
+			$this->basic_model->table_name = $this->table_name;
+			$this->basic_model->id_name = $this->id_name;
+
+			if ($return_boolean === FALSE):
+				return $result;
+			else:
+				return ( empty($result) )? FALSE: TRUE;
+			endif;
+		} // check_stuff
+
+	} // end class Account
 
 /* End of file Account.php */
 /* Location: ./application/controllers/Account.php */
