4b53fcd2b7b42c8fa5a6255d621771e86b98e723
Kamas "Iceberg" Lau <kamaslau@outlook.com>
2017-08-04 03:23:14 +0800

    beta20170804

--- application/controllers/Biz.php
+++ application/controllers/Biz.php
@@ -239,8 +239,8 @@
 			// 验证规则 https://www.codeigniter.com/user_guide/libraries/form_validation.html#rule-reference
 			$this->load->library('form_validation');
 			$this->form_validation->set_error_delimiters('', '');
-			$this->form_validation->set_rules('name', '商家全称', 'trim|required|min_length[7]|max_length[30]|is_unique[biz.name]');
-			$this->form_validation->set_rules('brief_name', '简称', 'trim|required|max_length[10]|is_unique[biz.brief_name]');
+			$this->form_validation->set_rules('name', '商家全称', 'trim|required|min_length[5]|max_length[35]|is_unique[biz.name]');
+			$this->form_validation->set_rules('brief_name', '简称', 'trim|required|max_length[15]|is_unique[biz.brief_name]');
 			$this->form_validation->set_rules('description', '简介', 'trim|max_length[200]');
 			$this->form_validation->set_rules('tel_public', '消费者联系电话', 'trim|required|min_length[10]|max_length[13]|is_unique[biz.tel_public]');
 			$this->form_validation->set_rules('tel_protected_biz', '商务联系手机号', 'trim|required|is_natural|exact_length[11]|is_unique[biz.tel_protected_biz]');
@@ -256,7 +256,7 @@
 			$this->form_validation->set_rules('url_image_owner_id', '法人身份证', 'trim|max_length[255]');
 			$this->form_validation->set_rules('url_image_auth_id', '经办人身份证', 'trim|max_length[255]');
 			$this->form_validation->set_rules('url_image_auth_doc', '经办人授权书', 'trim|max_length[255]');
-			
+
 			$this->form_validation->set_rules('bank_name', '开户行名称', 'trim|min_length[3]|max_length[20]');
 			$this->form_validation->set_rules('bank_account', '开户行账号', 'trim|max_length[30]|is_unique[biz.bank_account]');
 
@@ -399,8 +399,8 @@
 			$this->load->library('form_validation');
 			$this->form_validation->set_error_delimiters('', '');
 			if ($this->app_type === 'admin'):
-				$this->form_validation->set_rules('name', '商家名称', 'trim|required|min_length[7]|max_length[30]');
-				$this->form_validation->set_rules('brief_name', '简称', 'trim|required|max_length[10]');
+				$this->form_validation->set_rules('name', '商家名称', 'trim|required|min_length[5]|max_length[35]');
+				$this->form_validation->set_rules('brief_name', '简称', 'trim|required|max_length[15]');
 				$this->form_validation->set_rules('url_name', '店铺域名', 'trim|max_length[20]|alpha_dash');
 				$this->form_validation->set_rules('tel_protected_biz', '商务联系手机号', 'trim|required|exact_length[11]|is_natural');
 			endif;
@@ -511,8 +511,8 @@
 			$data_to_validate["{$name}"] = $value;
 			$this->form_validation->set_data($data_to_validate);
 			if ($this->app_type === 'admin'):
-				$this->form_validation->set_rules('name', '商家名称', 'trim|min_length[7]|max_length[30]');
-				$this->form_validation->set_rules('brief_name', '商家简称', 'trim|max_length[10]');
+				$this->form_validation->set_rules('name', '商家名称', 'trim|min_length[5]|max_length[35]');
+				$this->form_validation->set_rules('brief_name', '商家简称', 'trim|max_length[15]');
 				$this->form_validation->set_rules('url_name', '店铺域名', 'trim|max_length[20]|alpha_dash');
 				$this->form_validation->set_rules('tel_protected_biz', '商务联系手机号', 'trim|exact_length[11]|is_natural');
 			endif;
